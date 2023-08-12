8fedc91c9484ae684b5e0b91da505a26aa38dcc4
Wm <wojtek.mendlik@gmail.com>
2023-08-12 23:00:40 +0200

    ItWorksButNotHappyAboutIt

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,52 +6,52 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
- 
-
+    
     public float speed = 10;
     float jumpForce = 20;
     private float velocity;
     float gravity = -9.81f;
     private int howManyJumps;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-         
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && howManyJumps < 1)
-        {
-            velocity = jumpForce;
-            howManyJumps++;
-            
-        
-        }
-    }
-
+    [SerializeField] private Transform feet;
+    [SerializeField]  float floorHeight = 0.5f;
+    [SerializeField]  ContactFilter2D filter;
+    private bool isGrounded;
+    Collider2D[] results = new Collider2D[1];
+     
+ 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        
         Debug.Log(velocity);
-
-        velocity += gravity * 5 * Time.deltaTime;
-
+        PlayerJump();
         
-        if (Physics2D.Raycast(transform.position, Vector2.down, 0.8f) && velocity <0 )
+    }
+    
+    void PlayerJump()
+    {
+        velocity += gravity * 5 * Time.deltaTime;
+        
+        if (Physics2D.OverlapBox(feet.position, feet.localScale,0 , filter, results) > 0 && velocity <0)
         {
             velocity = 0;
-            howManyJumps = 0;
+            Vector2 surface = Physics2D.ClosestPoint(transform.position, results[0]) + Vector2.up * floorHeight;
+            transform.position = new Vector3(transform.position.x, surface.y, transform.position.z);
+            isGrounded = true;
         }
-       
-
+        else
+        {
+            isGrounded = false;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity = jumpForce;
+        }
+        
         float horizontalInput = Input.GetAxis("Horizontal")  * speed;
         transform.Translate(new Vector3(horizontalInput, velocity, 0) * Time.deltaTime);
-
- 
     }
-
-  
+   
+        
+        
 }
